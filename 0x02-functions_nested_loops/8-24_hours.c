05b7b555891e1b3318916a274426922167f9f2fc
DanMurigu <murigukingara@gmail.com>
2022-12-31 00:16:19 -0800

    Jack Bauer longest day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,40 +2,27 @@
 
 /**
  *Main - entry point
- * jack_bauer -> prints 24 hours
- *
+ * jack_bauer -> prints every minute of the day of jack baur
  */
 void jack_bauer(void)
 {
 	int i, j;
 
-	for (i = 0; i < 24; i++)
+	i = 0;
+	
+	while (i < 24)
 	{
-		for (j = 0; j < 60; j++)
+		j = 0;
+		while (j < 60)
 		{
-			if (i < 10)
-			{
-				_putchar('0');
-					_putchar(i + '0');
-			}
-			else if (i >= 10)
-			{
-				_putchar((i / 10) + '0');
+			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
-			}
-			if (j < 10)
-			{
-			_putchar(i);
 			_putchar(':');
-			_putchar(j + '0');
-			}
-	else if (j >= 10)
-	{
-		_putchar(':');
-		_putchar((i / 10) + '0');
-		_putchar((j % 10) + '0');
-	}
-	_putchar('\n'); 
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
